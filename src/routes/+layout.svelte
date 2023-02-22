e0d2adb9f7499a4ea26edba2844ae90c238646ea
kame <tentyo0620@icloud.com>
2023-02-22 20:35:19 +0900

    setup tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<header class="h-14" />
+
+<slot class="bg-[#e6f2ff] h-100vh w-full" />
+<footer class="footer footer-center p-4 bg-base-300 text-base-content">
+	<div>
+		<p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+	</div>
+</footer>
