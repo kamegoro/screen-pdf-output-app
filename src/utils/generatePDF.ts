dccfbd7ea3feccb5113d501c9a7aa74fc6120922
kame <tentyo0620@icloud.com>
2023-02-23 02:06:13 +0900

    Fixed sveltekit version

--- src/utils/generatePDF.ts
+++ src/utils/generatePDF.ts
@@ -1,6 +1,24 @@
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 
-const generatePdf = () => {
-	const jspdf = new jsPDF();
+// Canvasの切り抜き
+const clipCanvas = (baseCanvas: HTMLCanvasElement, pdfWidth: number, clipPdfHeight: number) => {
+	const clipCanvas = document.createElement('canvas');
+	const context = clipCanvas.getContext('2d');
 };
+
+const generatePdf = async (element: HTMLElement) => {
+	const doc = new jsPDF('p', 'mm', 'a4', true);
+
+	// A4サイズのPDFの高さ・横幅
+	const pdfWidth = doc.internal.pageSize.getWidth();
+	const pdfHeight = doc.internal.pageSize.getHeight();
+
+	const marginX = 10;
+	const pdfContentWidth = pdfWidth - 2 * marginX;
+
+	// ElementをCanvas化
+	const canvas = await html2canvas(element);
+};
+
+export { generatePdf };
